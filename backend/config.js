93841ee02efd9a40780ab96949912ddc1af7f237
kp_broz <abhimanyukudla@gmail.com>
2024-01-28 21:15:14 +0530

    added project files

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://kp:abcde@kp.hv1n37v.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
